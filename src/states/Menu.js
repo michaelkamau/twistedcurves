5291adbc1939bbd45ca024eb0e4a89ee3016ea6b
michaelkamau <tokamau@gmail.com>
2015-10-06 16:46:44 +0300

    Removed commented code for clarity

--- src/states/Menu.js
+++ src/states/Menu.js
@@ -3,6 +3,7 @@
  *
  **/
 var menu = function (game) {
+    //TODO : Remove this stuff. Not needed for single player mode.
     maxPlayers = 7;
     keys = [
         Phaser.Keyboard.W,
@@ -26,7 +27,7 @@ menu.prototype = {
         this.scale.forceOrientation(true);
 
         if (changeColor) {
-            chosenColor = this.game.rnd.integerInRange(0, 3);
+            chosenColor = this.game.rnd.integerInRange(0, bgColors.length - 1);
             colorHex = bgColors[chosenColor];
             colorHexDark = bgColorsDark[chosenColor];
             document.body.style.background = colorHex;
@@ -62,13 +63,6 @@ menu.prototype = {
         });
         ui.title.anchor.setTo(0.5, 0.5);
 
-        /*ui.beta = this.add.text(0,0, "BETA", {
-         font: "50px dosis",
-         fill: "#ffffff",
-         align: "center"
-         });
-         ui.beta.anchor.setTo(0.5,0.5);*/
-
         // Remove unneeded buttons
 
         // loading prompt
@@ -80,7 +74,6 @@ menu.prototype = {
         ui.loadingPrompt.body.angularVelocity = 300;
 
         // play text
-
         ui.playText = this.game.make.text(0,0,"play",{
             font : "120px dosis",
             align : "center",
@@ -100,44 +93,6 @@ menu.prototype = {
         ui.playButton.input.useHandCursor = true;
         clickButton(ui.playButton, this.singlePlayer, this);
 
-        ////Single Player
-        //ui.spButton = this.add.button(0, 0, "singleplayer_button");
-        //ui.spButton.anchor.setTo(0.5, 0.5);
-        //ui.spButton.input.useHandCursor = true;
-
-        //clickButton(ui.spButton, this.singlePlayer, this);
-
-        //Multiplayer
-        //ui.mpButton = this.add.button(0, 0, "multiplayer_button");
-        //ui.mpButton.anchor.setTo(0.5, 0.5);
-        //ui.mpButton.input.useHamdCursor = true;
-        //clickButton(ui.mpButton, this.multiplayer, this);
-
-
-        //SetKeys or leaderboards
-        //if (mobile) {
-        //    ui.leaderboard = this.add.button(0, 0, "leaderboard_button");
-        //    ui.leaderboard.anchor.setTo(0.5, 0.5);
-        //    ui.leaderboard.input.useHandCursor = true;
-        //    clickButton(ui.leaderboard, this.leaderboard, this);
-        //
-        //    /*ui.login = this.add.button(0,0,"login_button");
-        //     ui.login.anchor.setTo(1,1);
-        //     ui.login.input.useHandCursor = true;
-        //     clickButton(ui.login, this.login, this);
-        //
-        //     ui.loginText = this.add.text(0,0, "login", {
-        //     font: "40px dosis",
-        //     fill: colorHex,
-        //     align: "center"});
-        //     ui.loginText.anchor.set(1, 1);*/
-        //} else {
-        //    ui.keysButton = this.add.button(0, 0, "setkeys_button");
-        //    ui.keysButton.anchor.setTo(0.5, 0.5);
-        //    ui.keysButton.input.useHandCursor = true;
-        //    clickButton(ui.keysButton, this.setKeys, this);
-        //}
-
         //Stats
         ui.statsButton = this.add.button(0, 0, "stats_button");
         ui.statsButton.anchor.setTo(0.5, 0.5);
@@ -155,110 +110,19 @@ menu.prototype = {
             ui.audioButton.input.useHandCursor = true;
         }
 
-        if (mobile && !iapDone && platform === "android") {
-            //Donate
-            ui.donateButton = this.add.button(0, 0, "donate_button");
-            ui.donateButton.anchor.setTo(1, 1);
-            ui.donateButton.input.useHandCursor = true;
-            clickButton(ui.donateButton, this.donate, this);
-        }
-
         clickButton(ui.audioButton, this.muteSound, this);
 
         this.scale.refresh();
+
         //Place the menu buttons and labels on their correct positions
         this.setPositions();
-
-        if (mobile && firstTime) {
-            firstTime = false;
-            this.login();
-
-            if (platform === "android") {
-                initIAP();
-            }
-        }
-
-        /*if (mobile && socialService && socialService.isLoggedIn()) {
-         this.getAvatar();
-         }*/
+        
     },
 
-    /*getAvatar: function () {
-     var loader = new Phaser.Loader(this.game);
-     loader.image('avatar',"http://placekitten.com/g/300/300");
-     loader.onLoadComplete.addOnce(function () {
-     console.log('avatar');
-     var ui = this.ui;
-     ui.avatar = this.add.image(0, 0, 'avatar');
-     ui.avatar.width = 40;
-     ui.avatar.height = 40;
-     ui.avatar.anchor.set(0.5);
-     ui.loginText.setText("logout");
-     }.bind(this));
-     loader.start();
-     },*/
-
-    /* login: function (board) {
-     if (!socialService) {
-     var gp = Cocoon.Social.GooglePlayGames;
-     gp.init({});
-     socialService = gp.getSocialInterface();
-     } else {
-     if (socialService.isLoggedIn()) {
-     this.ui.loginText.setText("login");
-     if (this.ui.avatar) {
-     this.ui.avatar.destroy();
-     }
-     socialService.logout();
-     this.state.restart();
-     } else {
-     if (!socialService.isLoggedIn()) {
-     socialService.login(function(loggedIn, error) {
-     if (error) {
-     console.error("login error: " + error.message);
-     } else {
-     this.getAvatar();
-     }
-     }.bind(this));
-     }
-     }
-     }
-     },*/
-
-
-    login: function (board) {
-        if (!socialService) {
-            socialInit();
-
-            if (socialService && !socialService.isLoggedIn()) {
-                socialService.login(function (loggedIn, error) {
-                    if (error) {
-                        console.error("login error: " + error.message);
-                    }
-                }.bind(this));
-            }
-        }
-    },
 
     singlePlayer: function () {
         this.state.start("SinglePlayer", true, false);
 
-    },
-
-    multiplayer: function () {
-        if (mobile) {
-            var mode = new MPNormal(1, this.game);
-            this.game.state.start("PreloadGame", true, false, mode);
-        } else {
-            this.state.start("Multiplayer");
-        }
-
-    },
-
-    setKeys: function () {
-        if (!mobile) {
-            this.state.start("SetKeys");
-        }
     },
 
     leaderboard: function () {
@@ -267,10 +131,6 @@ menu.prototype = {
 
     stats: function () {
         this.state.start("Stats");
-    },
-
-    donate: function () {
-        iap();
     },
 
     muteSound: function () {
@@ -311,32 +171,6 @@ menu.prototype = {
         } else {
             ui.title.scale.set(1, 1);
         }
-
-        /*if (wOrientation == "portrait" && mobile) {
-         ui.beta.position.set(w2+160,h2*0.3+100);
-         } else {
-         ui.beta.position.set(w2+360,h2*0.3+100);
-         }*/
-
-      //  ui.spButton.position.set(w2 - 170, h2);
-
-      //  ui.mpButton.position.set(w2 + 170, h2);
-
-        //if (mobile) {
-        //   // ui.leaderboard.position.set(w2 + w2 / 2, 1.6 * h2)
-        //
-        //    /*ui.login.position.set(w2*2, h2*2);
-        //     ui.loginText.position.set(w2*2-30, h2*2);
-        //     if (ui.avatar) {
-        //     ui.avatar.position.set(w2*2-160, h2*2-ui.login.height*0.5);
-        //     }*/
-        //
-        //    if (platform == 'android' && !iapDone) {
-        //        ui.donateButton.position.set(2 * w2, 2 * h2);
-        //    }
-        //} else {
-        //    ui.keysButton.position.set(w2 + w2 / 2, 1.6 * h2);
-        //}
 
         // add the loading Prompt.
         ui.loadingPrompt.position.set(w2,h2);
